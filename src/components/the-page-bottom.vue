e1226354d581ff9a188f1656109186f76e93dda0
Gleb <glebchum17@gmail.com>
2022-11-25 15:07:56 +0300

    adapt

--- src/components/the-page-bottom.vue
+++ src/components/the-page-bottom.vue
@@ -15,33 +15,33 @@ footer {
   background-color: var(--primary-color);
 }
 
-@media screen and (max-width: 1440px) {
-  footer{
-    max-height: 10rem;
+/*@media screen and (max-width: 1440px) {*/
+/*  footer{*/
+/*    max-height: 10rem;*/
 
-  }
-}
+/*  }*/
+/*}*/
 
-@media screen and (max-width: 1200px) {
-  footer{
-    max-height: 8rem;
-  }
-}
+/*@media screen and (max-width: 1200px) {*/
+/*  footer{*/
+/*    max-height: 8rem;*/
+/*  }*/
+/*}*/
 
-@media screen and (max-width: 992px) {
-  footer{
-    max-height: 6rem;
-  }
-}
+/*@media screen and (max-width: 992px) {*/
+/*  footer{*/
+/*    max-height: 6rem;*/
+/*  }*/
+/*}*/
 
-@media screen and (max-width: 768px) {
-  footer{
-    max-height: 5rem;
-  }
-}
-@media screen and (max-width: 576px) {
-  footer{
-    max-height: none;
-  }
-}
+/*@media screen and (max-width: 768px) {*/
+/*  footer{*/
+/*    max-height: 5rem;*/
+/*  }*/
+/*}*/
+/*@media screen and (max-width: 576px) {*/
+/*  footer{*/
+/*    max-height: none;*/
+/*  }*/
+/*}*/
 </style>
